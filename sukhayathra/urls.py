c19d0aa2c3c7946c9ef0bf8d2ea087da114912c3
Sidharth Sasi <sidharthsasi04@gmail.com>
2022-08-19 14:05:21 +0530

    signin

--- sukhayathra/urls.py
+++ sukhayathra/urls.py
@@ -18,7 +18,8 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('home.urls'))
+    path('',include('home.urls')),
+    path('account/',include('account.urls')),
     
     
 ]
